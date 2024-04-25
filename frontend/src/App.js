5693dbef7975bb10b542d7be99251daa80aa4a71
garnetraven <corbinchristopher1@gmail.com>
2024-04-25 16:34:11 -0700

    add pages to react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,18 @@ import { Routes, Route, Link } from 'react-router-dom';
 
 import './App.css';
 import Home from './pages/home';
+import Login from './pages/login';
+import Dashboard from './pages/dashboard';
+import NoMatch from './pages/nomatch';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </div>
   );
