c8c99ba7859d915430a55d4f5c82cb8c03852a71
garnetraven <corbinchristopher1@gmail.com>
2024-04-25 16:27:04 -0700

    start to structure frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
+import { Routes, Route, Link } from 'react-router-dom';
+
 import './App.css';
+import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">    
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
