76ca2f8dfc05c24fc4398fb6aa799a463a197dcd
garnetraven <corbinchristopher1@gmail.com>
2024-05-10 09:06:44 -0700

    merge App.js into index.js for simplicity

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,3 +13,25 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+import { Routes, Route, Link } from 'react-router-dom';
+
+import Home from './pages/home';
+import Login from './pages/login';
+import Dashboard from './pages/dashboard';
+import NoMatch from './pages/nomatch';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
